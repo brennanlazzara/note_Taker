5084b39b30c79c9feabafbebde60e3eb9406cc6e
Your Brennan Predmore <brennnann@gmail.com>
2020-03-03 18:42:20 -0600

    routes directory init // express & nodemon modules installed // server.js move from develop directory

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 // Initialize the app and create a port
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-
+app.use(express.static("./public"));
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
